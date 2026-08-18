89101d70b44711e85300e65b3d6ab5111429a9be
Reet <reetbajaj046@gmail.com>
2026-08-18 22:52:48 +0530

    Day 9 questions completed

---
+++ Day-9/Q17.c
@@ -0,0 +1,33 @@
+// Q17. Write a program to find the roots of a quadratic equation and categorize them.
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float a, b, c, discriminant, root1, root2;
+
+    printf("Enter coefficients a, b and c: ");
+    scanf("%f %f %f", &a, &b, &c);
+
+    discriminant = b * b - 4 * a * c;
+
+    if (discriminant > 0) {
+        root1 = (-b + sqrt(discriminant)) / (2 * a);
+        root2 = (-b - sqrt(discriminant)) / (2 * a);
+
+        printf("Roots are real and distinct.\n");
+        printf("Root 1 = %.2f\n", root1);
+        printf("Root 2 = %.2f\n", root2);
+    }
+    else if (discriminant == 0) {
+        root1 = -b / (2 * a);
+
+        printf("Roots are real and equal.\n");
+        printf("Root = %.2f\n", root1);
+    }
+    else {
+        printf("Roots are imaginary.\n");
+    }
+
+    return 0;
+}
