6b03ee1e6871d8f97a3bc94edc3403ad8858b99c
reet-b-30 <reetbajaj046@gmail.com>
2026-08-23 23:50:05 +0530

    Create Q28.c

---
+++ Day-14/Q28.c
@@ -0,0 +1,21 @@
+/* Q28 Write a program to print the
+ product of even numbers from 1 to n.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    long long product = 1;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for (i = 2; i <= n; i += 2) {
+        product *= i;
+    }
+
+    printf("%lld", product);
+
+    return 0;
+}
