c23294075b437a5f28e10ccc98f9e05e1558be2d
reet-b-30 <reetbajaj046@gmail.com>
2026-08-24 22:21:54 +0530

    Create Q30.c

---
+++ Day-15/Q30.c
@@ -0,0 +1,22 @@
+/* Q30: Write a program to reverse a given number. */
+
+#include <stdio.h>
+
+int main()
+{
+    int n, rev = 0, rem;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while(n != 0)
+    {
+        rem = n % 10;
+        rev = rev * 10 + rem;
+        n = n / 10;
+    }
+
+    printf("%d", rev);
+
+    return 0;
+}
