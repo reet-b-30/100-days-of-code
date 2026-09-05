b09d63fb2be81e131ff81442b9728c2e58fe34d4
reet-b-30 <reetbajaj046@gmail.com>
2026-09-05 23:47:22 +0530

    Create Q53.c

---
+++ Day-27/Q53.c
@@ -0,0 +1,36 @@
+// Q53 (Nested Loops without Arrays/Strings)
+// Write a program to print the following pattern:
+//
+// *
+// ***
+// *****
+// *******
+// *********
+// *******
+// *****
+// ***
+// *
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j, stars;
+
+    for (i = 1; i <= 9; i++)
+    {
+        if (i <= 5)
+            stars = 2 * i - 1;
+        else
+            stars = 2 * (10 - i) - 1;
+
+        for (j = 1; j <= stars; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
