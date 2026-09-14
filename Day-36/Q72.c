7e70680a0a8e1716d25b31bf4799042cfce8487b
reet-b-30 <reetbajaj046@gmail.com>
2026-09-14 18:14:11 +0530

    Day 36 questions completed

---
+++ Day-36/Q72.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main()
+{
+    int a[10][10], rows, cols, i, j, sum=0;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    printf("Enter the elements of matrix:\n");
+    for(i=0; i<rows; i++)
+    {
+        for(j=0; j<cols; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i=0; i<rows; i++)
+    {
+        for(j=0; j<cols; j++)
+        {
+            sum = sum + a[i][j];
+        }
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
