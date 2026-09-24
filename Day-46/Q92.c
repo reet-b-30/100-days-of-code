263815df6385dd08002638655c0ae57ff309ed7b
reet-b-30 <reetbajaj046@gmail.com>
2026-09-24 21:21:43 +0530

    Day 46 questions completed

---
+++ Day-46/Q92.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main()
+{
+    char str[100];
+    int i, j, found = 0;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    for(i=0; str[i]!='\0'; i++)
+    {
+        for(j=i+1; str[j]!='\0'; j++)
+        {
+            if(str[i] == str[j])
+            {
+                printf("%c", str[i]);
+                found = 1;
+                break;
+            }
+        }
+
+        if(found == 1)
+        {
+            break;
+        }
+    }
+
+    if(found == 0)
+    {
+        printf("No repeating alphabet");
+    }
+
+    return 0;
+}
